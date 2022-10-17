6c07e9fda909c44fd3c081a7f0702f1f26c298e4
Gracy Sam <gracysam@gmail.com>
2022-10-17 11:51:52 -0400

    deep import for utils

--- src/deepClassifier/utils/__init__.py
+++ src/deepClassifier/utils/__init__.py
@@ -1,17 +1,1 @@
-import os
-import sys
-import logging
-
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-log_dir = "logs"
-log_filepath = os.path.join(log_dir, "running_logs.log")
-os.makdirs(log_dir, exists_ok=True) 
-logging.basicConfig(
-                level=logging.INFO,
-                format=logging_str,
-                handlers = [
-                    logging.StreamHandler(sys.stdout), #to print the msgs n terminal
-                    logging.FileHandler(log_filepath)
-
-                ])
-logger = logging.getLogger("deepClassifierLogger")
+from deepClassifier.utils.common import *
